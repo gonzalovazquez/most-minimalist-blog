07a072eee46ab13c459abd98c1cf2bbbbac583f4
gonzalovazquez <gonzalovazquez010@gmail.com>
2020-10-02 23:37:54 -0400

    feat: 🎸 add Firebase

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ import database
 
 app = Flask(__name__)
 
-database.create_tables()
-
-
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
-        database.create_entry(entry_content, datetime.datetime.today().strftime("%b %d"))
+        print(entry_content)
+        # database.create_entry(entry_content, datetime.datetime.today().strftime("%b %d"))
 
     return render_template("index.html", entries=database.retrieve_entries())
--- database.py
+++ database.py
@@ -1,22 +1,24 @@
-import sqlite3
+import pyrebase
 
-CREATE_TABLE = "CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT)"
-CREATE_ENTRY = "INSERT INTO entries VALUES (?, ?)"
-RETRIEVE_ENTRIES = "SELECT * FROM entries"
+config = {
+  "apiKey": " AIzaSyB6SKFYNWI9gpduiT2Qnvb-jP08rMF8yjs",
+  "authDomain": "minimalist-blog.firebaseapp.com",
+  "databaseURL": "https://minimalist-blog.firebaseio.com",
+  "storageBucket": "minimalist-blog.appspot.com"
+}
 
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
-def create_tables():
-    with sqlite3.connect("data.db") as connection:
-        connection.execute(CREATE_TABLE)
-
-
-def create_entry(content, date):
-    with sqlite3.connect("data.db") as connection:
-        connection.execute(CREATE_ENTRY, (content, date))
-
+def create_entry(name, body, date):
+    db.child("posts").push({
+        "name": name,
+        "body": body,
+        "date": date
+    })
+    print("Saving to database")
 
 def retrieve_entries():
-    with sqlite3.connect("data.db") as connection:
-        cursor = connection.cursor()
-        cursor.execute(RETRIEVE_ENTRIES)
-        return cursor.fetchall()
+    posts = db.child("posts").get()
+    print(posts.val())
+    return posts.val()
